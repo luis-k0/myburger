c999fd6a5ae1909dd4ccc5e99ffe59b17dc9e073
luisk <luisk.net@gmail.com>
2019-01-23 10:56:51 -0200

    lesson 116

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Aux from '../../hoc/Auxiliar'
+import Aux from '../../hoc/Auxiliar';
+import classes from './Layout.css';
 
 const layout = ( props ) => (
     <Aux>
         <div>
             Toobar, SideDrawer, Backdrop
         </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
