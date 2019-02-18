4e3806bb6e273ee1792826c155baf338fa387b38
luis-k0-janis <luisk.net@gmail.com>
2019-02-17 22:12:08 -0300

    lesson 369

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,7 +8,7 @@ import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
 import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
 import * as actions from "../../../store/actions/index";
-import { updateObject } from "../../../shared/utility";
+import { updateObject, checkValidity } from "../../../shared/utility";
 
 class ContactData extends Component {
   state = {
@@ -132,38 +132,39 @@ class ContactData extends Component {
     //   });
   };
 
-  checkValidity(value, rules) {
-    let isValid = true;
+  // moved to utility.js
+  // checkValidity(value, rules) {
+  //   let isValid = true;
 
-    // caso não tenha validação, caso do select, retorna valido/true
-    if (!rules) {
-      return true;
-    }
+  //   // caso não tenha validação, caso do select, retorna valido/true
+  //   if (!rules) {
+  //     return true;
+  //   }
 
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
+  //   if (rules.required) {
+  //     isValid = value.trim() !== "" && isValid;
+  //   }
 
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
+  //   if (rules.minLength) {
+  //     isValid = value.length >= rules.minLength && isValid;
+  //   }
 
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
+  //   if (rules.maxLength) {
+  //     isValid = value.length <= rules.maxLength && isValid;
+  //   }
 
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid;
-    }
+  //   if (rules.isEmail) {
+  //     const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  //     isValid = pattern.test(value) && isValid;
+  //   }
 
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid;
-    }
+  //   if (rules.isNumeric) {
+  //     const pattern = /^\d+$/;
+  //     isValid = pattern.test(value) && isValid;
+  //   }
 
-    return isValid;
-  }
+  //   return isValid;
+  // }
 
   inputChangedHandler = (event, inputIdentifier) => {
     // console.log(event.target.value);
@@ -193,7 +194,7 @@ class ContactData extends Component {
       this.state.orderForm[inputIdentifier],
       {
         value: event.target.value,
-        valid: this.checkValidity(
+        valid: checkValidity(
           event.target.value,
           this.state.orderForm[inputIdentifier].validation
         ),
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -5,3 +5,37 @@ export const updateObject = (oldObject, updatedProperties) => {
     ...updatedProperties
   };
 };
+
+// converting checkValidity to use in others components
+export const checkValidity = (value, rules) => {
+  let isValid = true;
+
+  // caso não tenha validação, caso do select, retorna valido/true
+  if (!rules) {
+    return true;
+  }
+
+  if (rules.required) {
+    isValid = value.trim() !== "" && isValid;
+  }
+
+  if (rules.minLength) {
+    isValid = value.length >= rules.minLength && isValid;
+  }
+
+  if (rules.maxLength) {
+    isValid = value.length <= rules.maxLength && isValid;
+  }
+
+  if (rules.isEmail) {
+    const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    isValid = pattern.test(value) && isValid;
+  }
+
+  if (rules.isNumeric) {
+    const pattern = /^\d+$/;
+    isValid = pattern.test(value) && isValid;
+  }
+
+  return isValid;
+};
